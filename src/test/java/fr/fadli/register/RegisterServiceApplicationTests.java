7a2208a8efab8ba9d896ace8b7068081f4663137
abdelouahad <wahidox@gmail.com>
2020-09-30 20:05:12 +0200

    AFA: add register service microservice in order to register all microservices using eureka

---
+++ src/test/java/fr/fadli/register/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fadli.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
